2db470b6bbb24813e330300bbeac2734e2f5e526
Chris AtLee <catlee@mozilla.com>
2011-06-23 14:12:47 -0400

    adding the agent script

--- modules/selfserve-agent/manifests/init.pp
+++ modules/selfserve-agent/manifests/init.pp
@@ -1,6 +1,9 @@
+# NB that selfserve-agent.ini still needs to be created manually since we
+# don't have a way of managing all the branch definitions
 class selfserve-agent {
     $plugins_dir = $nagios::service::plugins_dir
     $nagios_etcdir = $nagios::service::etcdir
+    $selfserve_dir = "/builds/buildbot/selfserve-agent"
     file {
         "/etc/init.d/selfserve-agent":
             source => "puppet:///modules/selfserve-agent/selfserve-agent.initd",
@@ -14,30 +17,38 @@ class selfserve-agent {
             mode => 644,
             owner => "root",
             group => "root";
+        "$selfserve_dir/run_agent.sh":
+            require => Exec["selfserve-virtualenv"],
+            content => template("selfserve-agent/run_agent.sh.erb"),
+            mode => 755,
+            owner => "cltbld",
+            group => "cltbld";
     }
     service {
         "selfserve-agent":
             require => File["/etc/init.d/selfserve-agent"],
+            ensure => running,
             enable => true;
     }
     exec {
-        "virtualenv":
+        "selfserve-virtualenv":
             require => Package["python26-virtualenv"],
-            creates => "/builds/buildbot/selfserve-agent/lib",
-            command => "/usr/bin/virtualenv-2.6 /builds/buildbot/selfserve-agent",
+            creates => "$selfserve_dir/lib",
+            command => "/usr/bin/virtualenv-2.6 $selfserve_dir",
             user => "cltbld";
         "clone-buildapi":
             require => [
                         Package["mercurial"],
                         Exec["virtualenv"],
                        ],
-            creates => "/builds/buildbot/selfserve-agent/buildapi",
-            command => "/usr/bin/hg clone http://hg.mozilla.org/build/buildapi /builds/buildbot/selfserve-agent/buildapi",
+            creates => "$selfserve_dir/buildapi",
+            command => "/usr/bin/hg clone http://hg.mozilla.org/build/buildapi $selfserve_dir/buildapi",
             user => "cltbld";
         "install-buildapi":
             require => Exec["clone-buildapi"],
-            creates => "/builds/buildbot/selfserve-agent/buildapi/lib/python2.6/site-packages/buildapi.egg-link",
-            command => "/builds/buildbot/selfserve-agent/bin/python /builds/buildbot/selfserve-agent/buildapi/setup.py develop",
+            creates => "$selfserve_dir/buildapi/lib/python2.6/site-packages/buildapi.egg-link",
+            command => "$selfserve_dir/bin/python $selfserve_dir/buildapi/setup.py develop",
+            cwd => $selfserve_dir,
             user => "cltbld";
     }
 }
