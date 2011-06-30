a801964eb1ffdb4507ac3a5c50a6a20dac1332b8
Chris AtLee <catlee@mozilla.com>
2011-06-30 17:21:12 -0400

    install dependencies

--- modules/buildapi/manifests/init.pp
+++ modules/buildapi/manifests/init.pp
@@ -2,10 +2,16 @@ class buildapi {
     package {
         "python2.6":
             ensure => latest;
+        "mysql-devel":
+            ensure => latest;
     }
     python::virtualenv {
         "/var/www/wsgi/buildapi":
-            require => Package["python2.6"],
+            require => [Package["python2.6"], Package["python2.6-devel"], Package["mysql-devel"]],
+            packages => [
+                "pylons",
+                "mysql-python",
+            ],
             python => "/usr/bin/python2.6";
     }
 }
