d34ec397ab7884e83df44181d75d3e916a4c5578
Chris AtLee <catlee@mozilla.com>
2011-06-30 16:56:24 -0400

    Setting up service

--- modules/redis/manifests/init.pp
+++ modules/redis/manifests/init.pp
@@ -3,4 +3,16 @@ class redis {
         "redis":
             ensure => latest;
     }
+    file {
+        "/etc/redis.conf":
+            requires => Package["redis"],
+            source => "puppet:///modules/redis/redis.conf";
+    }
+    service {
+        "redis":
+            requires => File["/etc/redis.conf"],
+            subscribe => File["/etc/redis.conf"],
+            enable => true,
+            ensure => running;
+    }
 }
